1ed7e0de414e4a5236883cb2aeff13bfc96bc73c
SamHn <shanini@adneom.com>
2017-10-06 00:27:25 +0200

    added intro game

--- animation_sprite_sheet.js
+++ animation_sprite_sheet.js
@@ -1,7 +1,12 @@
 //Creating animations from sprite sheets
-var sprite_sheet_image;
-var sprite_sheet;
-var explode_animation;
+// var sprite_sheet_image;
+// var sprite_sheet;
+// var explode_animation;
+
+let introImg;
+let introTimeout = 200;
+let isIntro = true;
+let introSprite;
 
 function preload() {
   // specify width and height of each frame and number of frames
@@ -10,14 +15,27 @@ function preload() {
 
   // load the full sprite sheet for example reference only
   // sprite_sheet_image = loadImage('assets/explode_sprite_sheet.png');
+  introImg = loadImage('assets/spyman-a-week-a-game.png');
 }
 
 function setup() {
-  createCanvas(800, 225);
-  background(0);
+  createCanvas(800, 400);
+  background(255);
 }
 
 function draw() {
+  if (frameCount%introTimeout === 0) {
+    console.log('false : ', isIntro);
+    isIntro = false;
+  }
+
+  if (isIntro) {
+    intro();
+  } else {
+    // introSprite.
+    launchGame();
+  }
+
   // clear();
 
   // animate the sprite sheet
@@ -25,4 +43,14 @@ function draw() {
 
   // show full sheet for example reference
   // image(sprite_sheet_image, 250, 40, 500, 154);
+}
+
+function launchGame() {
+  removeSprite(introSprite);//.remove();
+}
+
+function intro() {
+  introSprite = createSprite(0, 0);
+	introSprite.addImage(introImg);
+  drawSprites(introSprite);
 }
